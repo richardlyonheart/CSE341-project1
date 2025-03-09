726bffc5798138d29b264ec612e3cb171f01e2d6
richardlyonheart <brendenlyon907@gmail.com>
2025-03-08 20:51:32 -0500

    another fix for single data search

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,6 +1,6 @@
 const { response } = require('express');
 const mongodb = require('../data/database');
-const ObjectId = require('mongodb').ObjectId;
+const search = require('mongodb').ObjectId;
 
 const getAll = async (req, res) => {
     const result = await mongodb.getDatabase().db().collection('contacts').find();
@@ -11,7 +11,7 @@ const getAll = async (req, res) => {
 };
 
 const getSingle = async (req, res) => {
-    const userId = new ObjectId(req.params.id);
+    const userId = new search(req.params.id);
     const result = await mongodb.getDatabase().db().collection('contacts').find({_id: userId});
     result.toArray().then((contacts) => {
         res.setHeader('Content-Type', 'application/json');
--- routes/contacts.js
+++ routes/contacts.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const usersController = require('../controllers/contacts');
+const contactsController= require('../controllers/contacts');
 
-router.get('/', usersController.getAll);
+router.get('/', contactsController.getAll);
 
-router.get('/:id', usersController.getSingle);
+router.get('/:id', contactsController.getSingle);
 
 module.exports = router;
