dbe0e066ee6c6250eece88185b9f7f832ba886a8
richardlyonheart <brendenlyon907@gmail.com>
2025-03-09 18:01:21 -0400

    test

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -3,7 +3,7 @@ const mongodb = require('../data/database');
 const search = require('mongodb').ObjectId;
 
 const getAll = async (req, res) => {
-    const result = await mongodb.getDatabase().db(contacts).collection('contacts').find();
+    const result = await mongodb.getDatabase().db('contacts').collection('contacts').find();
     result.toArray().then((contacts) => {
         res.setHeader('Content-Type', 'application/json');
         res.status(200).json(contacts);
@@ -11,13 +11,17 @@ const getAll = async (req, res) => {
 };
 
 const getSingle = async (req, res) => {
-    const userId = new search(req.params.id);
-    const result = await mongodb.getDatabase().db().collection('contacts').find({_id: userId});
-    result.toArray().then((contacts) => {
+    try {
+        const userId = new mongodb.ObjectID(req.params.id); // Ensure ObjectId is correctly created
+        const result = await mongodb.getDatabase().db('contacts').collection('contacts').find({_id: userId});
+        const contacts = await result.toArray();
         res.setHeader('Content-Type', 'application/json');
-        res.status(200).json([0]);
-    });
+        res.status(200).json(contacts.length > 0 ? contacts : []);
+    } catch (error) {
+        res.status(500).json({ error: 'An error occurred while fetching the contact.' });
+    }
 };
+
 
 module.exports = {
     getAll,
