b231d1546e0b8c64c308abc15c5cdf29e810736f
richardlyonheart <brendenlyon907@gmail.com>
2025-03-08 19:30:45 -0500

    progress on week 1

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require('express');
+
+const mongodb = require('./data/database');
 const app = express();
+
 
 const PORT = process.env.PORT || 3000;
 
 app.use('/', require('./routes'));
 
+
+mongodb.initDb((err) => {
+    if(err) {
+        console.log(err);
+    }
+    else {
+        app.listen(PORT, () => {console.log(`Database is listening, and node is running ong port ${PORT}`)})
+    }
+})
 app.listen(PORT, () => {console.log(`running on port ${PORT}`)
 });
