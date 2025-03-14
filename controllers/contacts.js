b39d3c40f8f6fa775aa524f1e771165cfe1f784d
richardlyonheart <brendenlyon907@gmail.com>
2025-03-13 20:13:04 -0400

    fixed get single

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,5 +1,6 @@
 const { response } = require('express');
 const mongodb = require('../data/database');
+const ObjectId = require("mongodb").ObjectId;
 
 const getAll = async (req, res) => {
     const result = await mongodb.getDatabase().db('contacts').collection('contacts').find();
@@ -10,16 +11,17 @@ const getAll = async (req, res) => {
 };
 
 const getSingle = async (req, res) => {
-    try {
-        const contactId = new mongodb.ObjectID(req.params.id); // Ensure ObjectId is correctly created
-        const result = await mongodb.getDatabase().db('contacts').collection('contacts').find({_id: contactId});
-        const contacts = await result.toArray();
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(contacts[0]);
-    } catch (error) {
-        res.status(500).json({ error: 'An error occurred while fetching the contact.' });
-    }
-};
+    const contactId = new ObjectId(req.params.id);
+    const result = await mongodb
+      .getDatabase()
+      .db('contacts')
+      .collection("contacts")
+      .find({ _id: contactId });
+    result.toArray().then((contacts) => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(200).json(contacts[0]);
+    });
+  };
 
 
 module.exports = {
